ce59639fbeba68d30ee317a608e3177fdf910b2a
sethupathii <mysethu888@gmail.com>
2024-02-19 14:32:45 +0530

    Button Added

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Next</h1>
+    <div>
+      <h1>Albion Products</h1>
+      <Link href={'/users'}>
+        Go to Products
+      </Link>
+    </div>
+
   );
 }
